416a0bd9a4c463293dcc37c22c84730d94a67dbc
Lishakar <lishakarj@gmail.com>
2024-08-12 23:48:33 +0530

    Use Product.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,34 +1,4 @@
-console.log("Hello");
 
-const products = [
-    {
-        "image":"images/products/athletic-cotton-socks-6-pairs.jpg",
-        "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        "rating":{
-            "starts":4.5,
-            "count":87
-        },
-        "price": 1090
-     },
-     {
-        "image":"images/products/intermediate-composite-basketball.jpg",
-        "name":"Intermediate Size Basketball",
-        "rating":{
-            "starts":4.0,
-            "count":127
-        },
-        "price":2095
-     },
-     {
-        "image":"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        "name":"Adults Plain Cotton T-Shirt - 2 Pack",
-        "rating":{
-            "starts":4.5,
-            "count":56
-        },
-        "price":799
-     }
-];
 
 let productsHtml ='';
 
@@ -46,14 +16,14 @@ const html = `
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.starts * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.price/100).toFixed(2)}
+            $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
